7625fd4919e898fb72044e092f1a86243bc5f6a9
lauraanne123 <lauraanne123@gmail.com>
2026-07-05 11:09:05 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C="habbit-v1";
-self.addEventListener("install",e=>{self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==C).map(n=>caches.delete(n)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.open(C).then(c=>c.match(e.request).then(r=>{const n=fetch(e.request).then(res=>{if(res.ok)c.put(e.request,res.clone());return res;}).catch(()=>r);return r||n;})));});
